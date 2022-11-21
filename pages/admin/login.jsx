fd916767694b354bdb7c886f82de372ce8802497
SHARUN266 <sharunattari266@gmail.com>
2022-11-21 20:55:01 +0530

    pizzaName

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,52 +1,4 @@
-// import axios from "axios";
-// import { useRouter } from "next/router";
-// import React from "react";
-// import styles from "../../styles/Login.module.css";
-// function Login() {
-//   const [username, setUsername] = React.useState(null);
-//   const [password, setPassword] = React.useState(null);
-//   const [error, setError] = React.useState(false);
-//   const router = useRouter();
-//   const handleClick = async (e) => {
-//     try {
-//       await axios.post("http://localhost:3000/api/login", {
-//         username,
-//         password,
-//       });
-//        console.log("login success")
-//       router.push("/admin");
-//     } catch (err) {
-//       setError(true);
-//     }
-//   };
 
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.wrapper}>
-//         <h1>Admin Dashboard</h1>
-
-//         <input
-//           type="text"
-//           className={styles.input}
-//           placeholder="Username"
-//           onChange={(e) => setUsername(e.target.value)}
-//         />
-//         <input
-//           type="text"
-//           className={styles.input}
-//           placeholder="Password"
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <button onClick={handleClick} className={styles.button}>
-//           Sign In
-//         </button>
-//         {error && <span className={styles.error}>Wrong creadentials</span>}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -70,7 +22,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Yo Pizza!
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
